938eddecb5524786fd01135c52ba8d73f506a552
Marcelo Lima <marcelo_sp5@hotmail.com>
2020-03-21 15:40:56 -0300

    Styled headers, lists and cards

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Label } from './styles';
 
 export default function Card() {
   return (
-    <div />
+    <Container>
+      <header>
+        <Label color='#7159C1' />
+      </header>
+      <p>Fazer a migração completa do servidor</p>
+      <img
+        src='https://api.adorable.io/avatars/40/abott@adorable'
+        alt='Avatar'
+      />
+    </Container>
   );
 }
